8aee6f26a356d99de5bb54cc711e07dab3e12cc7
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2023-02-27 16:18:08 +0100

    Add package

--- status2023feb/packages.R
+++ status2023feb/packages.R
@@ -1,3 +1,5 @@
 source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
-pkgs <- c("xaringan", "xaringanExtra", "xaringanthemer")
+pkgs <- c("xaringan", "xaringanExtra", "xaringanthemer", "devtools")
 kh_load(char = pkgs)
+
+devtools::install_github("hadley/emo")
